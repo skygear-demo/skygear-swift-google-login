1cf89b1534b518abdfab8531cef5384dae1aee59
Elliot Ng <chihimng.io@gmail.com>
2018-06-16 19:44:48 +0800

    feat(profile-view): implement ui

---
+++ SkygearGoogleOAuthDemo/ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  SkygearGoogleOAuthDemo
+//
+//  Created by Chihim Ng on 16/6/2018.
+//  Copyright © 2018 SkygearIO. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
