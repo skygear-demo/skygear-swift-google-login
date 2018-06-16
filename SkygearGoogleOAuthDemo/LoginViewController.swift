88b2d96f085c5184fa8d0cee9c37dd1dda313254
Elliot Ng <chihimng.io@gmail.com>
2018-06-16 19:28:37 +0800

    feat(login-view): implement ui

---
+++ SkygearGoogleOAuthDemo/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  SkygearGoogleOAuthDemo
+//
+//  Created by Chihim Ng on 16/6/2018.
+//  Copyright © 2018 SkygearIO. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+
+    @IBAction func loginButtonPressed(_ sender: Any) {
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
